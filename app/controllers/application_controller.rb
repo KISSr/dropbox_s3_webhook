6739eba58f9707ce9c02a316da2af19d6d2ba302
Mason Fischer <mason@thoughtbot.com>
2014-03-22 22:23:22 -0400

    Use :null_session when protecting from csrf

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,9 @@
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
+  protect_from_forgery with: :null_session
 
   before_filter :authorize
 
   def authorize
-    binding.pry
     unless valid_signature?
       render nothing: true, status: 401
     end

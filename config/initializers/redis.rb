327f163a0e64145c81861cd46008cad80b7e3141
Mason Fischer <mason@thoughtbot.com>
2014-03-22 23:23:01 -0400

    Configure redis

---
+++ config/initializers/redis.rb
@@ -0,0 +1,9 @@
+if ENV["REDISTOGO_URL"].present?
+  uri = URI.parse(ENV["REDISTOGO_URL"])
+  Resque.redis = Redis.new(
+    host: uri.host,
+    port: uri.port,
+    password: uri.password,
+    thread_safe: true
+  )
+end

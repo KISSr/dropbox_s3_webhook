8bba7f453e226226b82fe21cb9da3a54ea8a481b
Mason Fischer <mason@thoughtbot.com>
2014-03-23 00:00:36 -0400

    Fix delete bug

--- app/models/delta_job.rb
+++ app/models/delta_job.rb
@@ -5,26 +5,31 @@ class DeltaJob
     @@user = User.find_by(dropbox_user_id: dropbox_user_id)
 
     if @@user.present?
-      delta = dropbox.delta(@@user.cursor)
 
-      @@user.update_attributes(cursor: delta['cursor'])
+      begin
+        delta = dropbox.delta(@@user.cursor)
 
-      process_delta(delta['entries'])
+        process_delta(delta['entries'])
+
+        @@user.cursor = delta['cursor']
+      end while delta['has_more']
+
+      @@user.save
     end
   end
 
   def self.process_delta(delta)
     delta.each do |file_name, metadata|
-      if metadata['is_dir']
+      if metadata.nil?
+        bucket.files.new(key: file_name[1..-1]).destroy
+      elsif metadata['is_dir'].present?
         next
-      elsif metadata.present?
+      else
         bucket.files.create(
-          key: file_name,
+          key: file_name[1..-1],
           body: dropbox.get_file(file_name),
           public: true
         )
-      else
-        bucket.files.new(key: file_name).destroy
       end
     end
   end

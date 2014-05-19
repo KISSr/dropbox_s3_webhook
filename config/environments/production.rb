88af60b4ea958627beb542bd31acf159b8a17758
Mason Fischer <mason@thoughtbot.com>
2014-05-19 14:38:05 -0700

    Inline

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,5 @@ DropboxS3Webhook::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+  Resque.inline = true
 end
